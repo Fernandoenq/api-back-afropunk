f5322daf41e1b88302da0f281ef2760e4f49b2e9
Fernando <fernandoasfilho74@gmail.com>
2024-11-09 20:52:19 -0300

    voltei pra teste

--- Application/Controllers/ImageController.py
+++ Application/Controllers/ImageController.py
@@ -10,27 +10,27 @@ class ImageController:
     def setup_controller(app):
         @app.route('/Image/Image', methods=['GET'])
         def get_image_ids():
+            #try:
+            connection = ConnectionService.open_connection()
+            cursor = connection.cursor()
+            connection.start_transaction()
+
             try:
-                connection = ConnectionService.open_connection()
-                cursor = connection.cursor()
-                connection.start_transaction()
+                image_ids = ImageService().get_image_ids(cursor)
 
-                try:
-                    image_ids = ImageService().get_image_ids(cursor)
+                response_json = {
+                    "image_ids": image_ids
+                }
 
-                    response_json = {
-                        "image_ids": image_ids
-                    }
+                return jsonify(response_json), 200
 
-                    return jsonify(response_json), 200
-
-                except Exception as e:
-                    if connection.is_connected():
-                        connection.rollback()
-                    error_response = ErrorResponseModel(Errors=[f"{str(e)} | {traceback.format_exc()}"])
-                    return jsonify(error_response.dict()), 500
-                finally:
-                    if connection.is_connected():
-                        ConnectionService.close_connection(cursor, connection)
             except Exception as e:
-                return jsonify('Erro servidor'), 500
+                if connection.is_connected():
+                    connection.rollback()
+                error_response = ErrorResponseModel(Errors=[f"{str(e)} | {traceback.format_exc()}"])
+                return jsonify(error_response.dict()), 500
+            finally:
+                if connection.is_connected():
+                    ConnectionService.close_connection(cursor, connection)
+            #except Exception as e:
+            #    return jsonify('Erro servidor'), 500
